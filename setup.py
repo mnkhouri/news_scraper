fca293da2b37f417d42d96eb026ccc9d20e12420
Marc Khouri <mkhouri@wink.com>
2015-07-20 00:27:59 -0600

    Changes package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='news_scraper',
+      version='0.0.1',
+      description='Generates short snippets of news articles',
+      url='http://github.com/mnkhouri/news_scraper',
+      author='Marc Khouri',
+      author_email='marc@khouri.ca',
+      license='GPLv2',
+      packages=['news_scraper'],
+      zip_safe=False)
