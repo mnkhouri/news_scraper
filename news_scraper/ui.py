f758a675f84d443cd692ca3b8281c54ea497fc14
Marc Khouri <mkhouri@wink.com>
2015-07-23 00:42:59 -0600

    Move UI stuff around

--- news_scraper/ui.py
+++ news_scraper/ui.py
@@ -1,6 +1,5 @@
 import codecs
 import traceback
-import collections
 import time
 import sys
 
@@ -10,6 +9,32 @@ from . import display
 from . import scrape
 
 
+def _get_article(url, bodyLines=4, debug=False):
+    try:
+        article = scrape.fetch_and_parse(url, bodyLines)
+    except NameError:
+        if debug:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_exception(exc_type, exc_value, exc_traceback)
+        print('========= ERROR! =========\nNews source not programmed: ' + url + '\n')
+    except Exception:
+        print('========= ERROR! =========\nArticle cannot be parsed: ' + url + '\n')
+        if debug:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_exception(exc_type, exc_value, exc_traceback)
+    else:
+        print('Got article: "' + article.headline + '"\n')
+        return article
+
+
+def _output(articles, outputFile, failures, failureFile):
+    #display.output_to_term(articles)
+    display.output_to_html(articles, outputFile)
+    display.open_file(outputFile)
+    with codecs.open(failureFile, encoding='utf-8', mode='w') as output:
+        output.write('\n'.join(failures))
+
+
 def mode_interactive(options):
     """Interactive Mode: terminal prompts for a source then repeatedly for a url"""
     articles = []
@@ -17,29 +42,14 @@ def mode_interactive(options):
 
     url = input('Enter a URL: ')
     while url != '':
-        try:
-            article = scrape.fetch_and_parse(url, options.bodyLines)
-        except NameError:
-            if options.debug:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback)
-            print('========= ERROR! =========\nNews source not programmed: ' + url + '\n')
-        except Exception:
-            print('========= ERROR! =========\nArticle cannot be parsed: ' + url + '\n')
-            failures.append(url)
-            if options.debug:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback)
-        else:
+        article = _get_article(url, options.bodyLines, options.debug)
+        if (article):
             articles.append(article)
-            print('Got article: "' + article.headline + '"\n')
+        else:
+            failures.append(url)
         url = input("Enter a URL (press enter to end): ")
 
-    #display.output_to_term(articles)
-    display.output_to_html(articles, options.outputFile)
-    display.open_file(options.outputFile)
-    with codecs.open(options.failureFile, encoding='utf-8', mode='w') as output:
-        output.write('\n'.join(failures))
+    _output(articles, options.outputFile, failures, options.failureFile)
 
 
 def mode_clipboard_watch(options):
@@ -57,29 +67,15 @@ def mode_clipboard_watch(options):
                 url = tmp_value
                 print("Fetching article...")
                 if options.debug:
-                    print("Value changed: %s" % str(url)[:30])
-                try:
-                    article = scrape.fetch_and_parse(url, options.bodyLines)
-                except NameError:
-                    if options.debug:
-                        exc_type, exc_value, exc_traceback = sys.exc_info()
-                        traceback.print_exception(exc_type, exc_value, exc_traceback)
-                    print('========= ERROR! =========\nNews source not programmed: ' + url + '\n')
-                except Exception:
-                    print('========= ERROR! =========\nArticle cannot be parsed: ' + url + '\n')
-                    failures.append(url)
-                    if options.debug:
-                        exc_type, exc_value, exc_traceback = sys.exc_info()
-                        traceback.print_exception(exc_type, exc_value, exc_traceback)
-                else:
+                    print("Value changed: %s" % str(url)[:100])
+
+                article = _get_article(url, options.bodyLines, options.debug)
+                if (article):
                     articles.append(article)
-                    print('Got article: "' + article.headline + '"\n')
-            time.sleep(0.1)
+                else:
+                    failures.append(url)
+                    time.sleep(0.1)
         except KeyboardInterrupt:
             break
 
-    #display.output_to_term(articles)
-    display.output_to_html(articles, options.outputFile)
-    display.open_file(options.outputFile)
-    with codecs.open(options.failureFile, encoding='utf-8', mode='w') as output:
-        output.write('\n'.join(failures))
+    _output(articles, options.outputFile, failures, options.failureFile)
--- tests/test_display.py
+++ tests/test_display.py
@@ -1,5 +1,1 @@
 import news_scraper.display as display
-
-
-def test_set_from_dict_attribute():
-    pass
